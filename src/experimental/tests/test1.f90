7dd15af7534689dc70fdabddeb80f3fe7b2cfc45
rasmussn <rasmus@uoregon.edu>
2013-02-12 15:21:31 -0800

    Initial version.

---
+++ src/experimental/tests/test1.f90
@@ -0,0 +1,38 @@
+
+var fact;
+
+fact := (fact);
+
+fact := .negate. 3;
+fact := 10**2;
+fact := 1 * 3;
+fact := 3 / 1;
+fact := 1 + 3;
+fact := 1 - 3;
+
+fact := +1;
+fact := -1;
+
+fact := 1 // 3;
+
+fact := 1 == 3;
+fact := 1 .EQ. 3;
+fact := 1 /= 3;
+fact := 1 .NE. 3;
+fact := 1 < 3;
+fact := 1 .lt. 3;
+fact := 1 <= 3;
+fact := 1 .le. 3;
+fact := 1 > 3;
+fact := 1 .gt. 3;
+fact := 1 >= 3;
+fact := 1 .ge. 3;
+
+fact := .not. 3;
+fact := 1 .and. 3;
+fact := 1 .OR. 3;
+
+fact := 1 .eqv. 3;
+fact := 1 .neqv. 3;
+
+fact := 1 .UNION. 3;
