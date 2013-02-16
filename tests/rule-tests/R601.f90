911e7c228b663fff1ae985d83b829f098838009d
rasmussn <rasmus@uoregon.edu>
2013-02-16 12:14:46 -0800

    Initial version.

---
+++ tests/rule-tests/R601.f90
@@ -0,0 +1,20 @@
+!! R601 designator
+!    is  object-name
+!    or  array-element
+!    or  array-section
+!    or  complex-part-designator
+!    or  structure-component
+!    or  substring
+! 
+! Full tests for each of the alternatives is done separately.  
+!
+! designator tested as part of a variable (R602), which is used as part of 
+! an assignment-stmt (R734).
+
+a = 1
+a(1) = 1
+a(1:2) = 1
+a%b = 1
+
+end
+
