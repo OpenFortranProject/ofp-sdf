1110118652f9769ae0d1d4630b40801df2507c54
rasmussn <rasmus@uoregon.edu>
2013-02-16 16:57:20 -0800

    Initial version.

---
+++ tests/rule-tests/R609.f90
@@ -0,0 +1,12 @@
+!! R609 substring
+!    is  parent-string ( substring-range )
+!
+! Tested separately are: R610 (parent-string) and R611 (substring-range).
+!
+! substring tested as part of a designator, which can be part of a primary, 
+! so tested as a rhs to an assignment-stmt.
+
+a = my_string(2:3)
+
+end
+
