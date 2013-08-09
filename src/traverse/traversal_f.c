6d50bc6b7eb24dbe965026de377553f4e12640da
rasmussn <rasmus@uoregon.edu>
2013-08-09 11:17:33 -0700

    Lot's of modifications for an OptionOption type kind.  Allows OptProgramName1 (for example) to call production for ProgramName.

--- src/traverse/traversal_f.c
+++ src/traverse/traversal_f.c
@@ -6,50 +6,60 @@
 
 ATermList gTypeTable;
 
-char * ofp_getArgName(ATerm list, char ** name)
+char * ofp_getArgNameStr(ATerm arg, char ** name)
 {
-   ATerm aType;
-   if (ATmatch(list, "[<term>,<str>]", &aType, name)) {
-      // MATCHED name
-   }
-   else {
-      printf("\nofp_getArgName: Error reading name from %s\n", ATwriteToString(list));
-      exit(1);
-   }
+   ATerm kind;
+   assert(ATmatch(arg, "[<term>,<str>]", &kind, name));
    return *name;
 }
 
-ATbool ofp_isArgListType(ATerm list)
+ATerm ofp_getArgName(ATerm arg)
 {
-   char * type;
-   ATerm aType, aName;
-   if (ATmatch(list, "[<str>,<term>]", &type, &aName)) {
-      // MATCHED name
-   }
-   else {
-      printf("\nofp_isArgListType: Error reading type from %s\n", ATwriteToString(list));
-      exit(1);
-   }
+   ATerm kind, name;
+   assert(ATmatch(arg, "[<term>,<term>]", &kind, &name));
+   return name;
+}
 
-   if (strncmp("List", type, 4) == 0) {
+ATerm ofp_getArgKind(ATerm arg)
+{
+   ATerm kind, name;
+   assert(ATmatch(arg, "[<term>,<term>]", &kind, &name));
+   return kind;
+}
+
+ATbool ofp_isArgListKind(ATerm list)
+{
+   ATerm kind, name;
+   assert(ATmatch(list, "[<term>,<term>]", &kind, &name));
+   if (ATmatch(kind, "\"List\"")) {
       return ATtrue;
    }
    return ATfalse;
 }
 
-ATbool ofp_isArgOptionType(ATerm list)
+ATbool ofp_isArgOptionKind(ATerm list)
 {
-   char * kind;
-   ATerm aName;
-   if (ATmatch(list, "[<str>,<term>]", &kind, &aName)) {
-      // MATCHED name
+   ATerm kind, name;
+   assert(ATmatch(list, "[<term>,<term>]", &kind, &name));
+   if (ATmatch(kind, "\"Option\"")) {
+      return ATtrue;
    }
-   else {
-      printf("\nofp_isArgOptionType: Error reading kind from %s\n", ATwriteToString(list));
-      exit(1);
-   }
+   return ATfalse;
+}
 
-   if (strncmp("Option", kind, 6) == 0) {
+ATbool ofp_isArgOptionOptionKind(ATerm list)
+{
+   ATerm kind, name;
+   assert(ATmatch(list, "[<term>,<term>]", &kind, &name));
+   if (ATmatch(kind, "\"OptionOption\"")) {
+      return ATtrue;
+   }
+   return ATfalse;
+}
+
+ATbool ofp_isOptionOrKind(ATerm kind)
+{
+   if (ATmatch(kind, "\"Option\"") || ATmatch(kind, "\"Or\"")) {
       return ATtrue;
    }
    return ATfalse;
@@ -63,83 +73,165 @@ ATbool ofp_isStringType(ATerm term)
    return ATfalse;
 }
 
-ATerm ofp_getArgType(ATerm term)
+/**
+ * Return a list of types associated with this name or None
+ */
+ATerm ofp_getTypeList(ATerm name, ATbool * isOptType)
+{
+   int i;
+
+   for (i = 0; i <  ATgetLength(gTypeTable); i++) {
+      ATerm typeName, typeList;
+      ATbool matched = ATfalse;
+      ATerm name_type = ATelementAt(gTypeTable, i);
+      if (ATmatch(name_type, "Type(<term>,<term>)", &typeName, &typeList)) {
+         *isOptType = ATfalse;
+         matched = ATtrue;
+      }
+      else if (ATmatch(name_type, "OptType(<term>,<term>)", &typeName, &typeList)) {
+         *isOptType = ATtrue;
+         matched = ATtrue;
+      }
+      if (matched && ATisEqual(name, typeName)) {
+         return typeList;
+      }
+   }
+
+   *isOptType = ATfalse;
+   return ATmake("None");
+}
+
+ATbool ofp_isTypeNameList(ATerm type)
+{
+   if (! ATmatch(type, "None")) {
+      ATermList nameList = (ATermList) type;
+      if (ATgetLength((ATermList) type) > 1) {
+         return ATtrue;
+      }
+   }
+   return ATfalse;
+}
+
+ATerm ofp_getArgType(ATerm term, ATbool * isOptType)
 {
    int i;
    ATerm kind, name;
 
    if (ATmatch(term, "[<term>,<term>]", &kind, &name)) {
-      // MATCHED name
-   } else return ATmake("None");
-
+      // MATCHED (kind, name)
+   }
+   else {
+      *isOptType = ATfalse;
+      return ATmake("None");
+   }
 
    for (i = 0; i <  ATgetLength(gTypeTable); i++) {
       ATerm typeName, typeList;
+      ATbool matched = ATfalse;
       ATerm name_type = ATelementAt(gTypeTable, i);
       if (ATmatch(name_type, "Type(<term>,<term>)", &typeName, &typeList)) {
-         if (ATisEqual(name, typeName)) {
-            return typeList;
-         }
+         matched = ATtrue;
+         *isOptType = ATfalse;
+      }
+      else if (ATmatch(name_type, "OptType(<term>,<term>)", &typeName, &typeList)) {
+         matched = ATtrue;
+         *isOptType = ATtrue;
+      }
+      if (matched && ATisEqual(name, typeName)) {
+         return typeList;
       }
    }
 
+   *isOptType = ATfalse;
    return ATmake("None");
 }
 
-/** Make traversal call for a production
+/** Make traversal calls for a production
  */
-ATbool ofp_build_node_traversal(ATerm term)
+ATbool ofp_build_node_traversal(ATerm name, ATerm arg, ATerm kind)
 {
    ATerm typeList;
-   ATbool option_type;
-   char * arg_name = "None";
+   ATbool isOptType;
+   char * root_name = "None";
+   char * arg_name  = "None";
 
-   option_type = ofp_isArgOptionType(term);
-   arg_name = ofp_getArgName(term, &arg_name);
-   typeList = ofp_getArgType(term);
+   arg_name = ofp_getArgNameStr(arg, &arg_name);
+   typeList = ofp_getArgType(arg, &isOptType);
 
-   if (ofp_isArgListType(term)) {
+   assert(ATmatch(name, "<str>", &root_name));
+   if (isOptType) {
+      // Some optional productions lead to temporary terms that can be reduced to (Some(<term>)).
+      // For example, ('PROGRAM' ProgramName?)? in EndProgramStmt produces
+      // Some((Some(ProgramName("main")))). Get the argname from the type namelist.
+      //
+      assert(ATmatch(typeList, "[<str>]", &arg_name));
+   }
+
+   // handle primitive types and terminals
+   //
+
+   if (ATisEqual(ofp_getArgName(arg), ATmake("\"Ident\""))) {
+      printf("::::::::::::found Ident:::::::: %s\n", ATwriteToString(arg));
+      printf("      char * %s_val;\n", arg_name);
+      printf("      if (ATmatch(%s_term.term, ", arg_name);
+      printf("\"<str>\"");
+      printf(", &%s_val)) {\n", arg_name);
+      printf("         // MATCHED %s\n", arg_name);
+      printf("      } else return ATfalse;\n\n");
+   }
+
+   else if (ofp_isStringType(typeList)) {
+      printf("      char * %s_val;\n", arg_name);
+      printf("      if (ATmatch(%s_term.term, ", arg_name);
+      if (ofp_isArgOptionKind(arg)) printf("\"Some(<str>)\"");
+      else                          printf("\"<str>\"");
+      printf(", &%s_val)) {\n", arg_name);
+      printf("         // MATCHED %s\n", arg_name);
+      if (ofp_isArgOptionKind(arg))  printf("      }\n\n");
+      else                           printf("      } else return ATfalse;\n\n");
+   }
+
+   // handle list type
+   //
+   else if (ofp_isArgListKind(arg)) {
+      printf("      OFP_Traverse %s;\n", arg_name);
       printf("      ATermList %s_tail = (ATermList) ATmake(\"<term>\", %s_list.term);\n", arg_name, arg_name);
       printf("      while (! ATisEmpty(%s_tail)) {\n", arg_name);
-      printf("         OFP_Traverse %s;\n", arg_name);
       printf("         %s.term = ATgetFirst(%s_tail);\n", arg_name, arg_name);
       printf("         %s_tail = ATgetNext(%s_tail);\n", arg_name, arg_name);
       printf("         if (ofp_traverse_%s(%s.term, &%s)) {\n", arg_name, arg_name, arg_name);
       printf("            // MATCHED %s\n", arg_name);
-      printf("         } else return ATfalse;\n");
+      if (! ATmatch(kind, "\"Or\""))  printf("         } else return ATfalse;\n");
+      else                            printf("         }\n");
       printf("      }\n");
    }
-   else if (ofp_isStringType(typeList)) {
-      printf("      char * str;\n");
-      printf("      if (ATmatch(%s.term, ", arg_name);
-      if (option_type) {
-         printf("\"Some(<str>)\"");
-      }
-      else {
-         printf("\"<str>\"");
-      }
-      printf(", &str)) {\n");
-      printf("         // MATCHED %s\n", arg_name);
-      printf("      } else return ATfalse;\n\n");
-   }
-   else {
-      printf("      if (ofp_traverse_%s(%s.term, &%s)) {\n", arg_name, arg_name, arg_name);
-      printf("         // MATCHED %s\n", arg_name);
-      printf("      } else return ATfalse;\n\n");
-   }
 
-      //      if (option_type) {
-      //         printf("Some(<term>)\"");
-      //      }
-      //      else {
-      //         printf("<term>\"");
-      //      }
+   // handle generic type
+   //
+   else {
+      printf("      OFP_Traverse %s;\n", arg_name);
+      printf("      if (ATmatch(%s_term.term, ", root_name);
+      if (ofp_isArgOptionOptionKind(arg)) printf("\"Some((Some(<term>)))\"");
+      else if (ofp_isArgOptionKind(arg))  printf("\"Some(<term>)\"");
+      else                                printf("\"<term>\"");
+      printf(", &%s.term)) {\n", arg_name);
+
+      printf("         if (ofp_traverse_%s(%s.term, &%s)) {\n", arg_name, arg_name, arg_name);
+      printf("            // MATCHED %s\n", arg_name);
+      if (ATmatch(kind, "\"Or\""))  printf("            return ATtrue;\n");
+      printf("         } else return ATfalse;\n");
+
+      if (ofp_isOptionOrKind(kind))  printf("      }\n\n");
+      else                           printf("      } else return ATfalse;\n\n");
+   }
 
    return ATtrue;
 }
 
 ATbool ofp_traverse_FunType_arg(ATerm term, pOFP_Traverse FunType_arg)
 {
+   ATbool isOptType;
+
 #ifdef DEBUG_PRINT
    printf("\nFunType_arg: %s\n", ATwriteToString(term));
 #endif
@@ -147,11 +239,26 @@ ATbool ofp_traverse_FunType_arg(ATerm term, pOFP_Traverse FunType_arg)
    if (ATmatch(term, "ConstType(Sort(<str>,<term>))", &FunType_arg->pre, &FunType_arg->term)) {
       if (ATmatch(FunType_arg->term, "[SortNoArgs(<str>)]", &FunType_arg->post)) {
          // MATCHED FunType_arg_type
+         ATerm type_name = ATmake("<str>", (char*)FunType_arg->post);
+         ATerm type_list = ofp_getTypeList(type_name, &isOptType);
+         if (isOptType) {
+            FunType_arg->pre = "OptionOption";
+         }
+
       } else return ATfalse;
    }
    else if (ATmatch(term, "ConstType(SortNoArgs(<str>))", &FunType_arg->post)) {
       // MATCHED FunType_arg_type
+      ATerm type_name = ATmake("<str>", (char*)FunType_arg->post);
+      ATerm type_list = ofp_getTypeList(type_name, &isOptType);
+
       FunType_arg->pre = "None";
+      if (! ATmatch(type_list, "None")) {
+         ATermList nameList = (ATermList) type_list;
+         if (ATgetLength(nameList) > 1) {
+            FunType_arg->pre = "Or";
+         }
+      }
    }
    else {
       return ATfalse;
@@ -190,10 +297,12 @@ ATbool ofp_traverse_FunType(ATerm term, pOFP_Traverse FunType)
    OFP_Traverse FunType_args, FunType_result;
    if (ATmatch(term, "FunType(<term>,<term>)", &FunType_args.term, &FunType_result.term) ) {
 
+   printf("---------args------- %s\n", ATwriteToString(FunType_args.term));
       if (ofp_traverse_FunType_result(FunType_result.term, &FunType_result)) {
          // MATCHED FunType_result
          name = (char*) FunType_result.post;
       } else return ATfalse;
+      printf("---------name------- %s\n", name);
 
       ATermList FunType_args_tail = (ATermList) ATmake("<term>", FunType_args.term);
       while (! ATisEmpty(FunType_args_tail)) {
@@ -228,9 +337,10 @@ ATbool ofp_traverse_FunType(ATerm term, pOFP_Traverse FunType)
    for (i = 0; i <  ATgetLength(args); i++) {
       char * arg_name = "None";
       ATerm arg = ATelementAt(args, i);
-      ATbool list_type = ofp_isArgListType(arg);
-      printf("%s %s", comma, ofp_getArgName(arg, &arg_name));
+      ATbool list_type = ofp_isArgListKind(arg);
+      printf("%s %s", comma, ofp_getArgNameStr(arg, &arg_name));
       if (list_type) printf("_list");
+      else           printf("_term");
       comma = ",";
    }
    printf(";\n");
@@ -238,7 +348,8 @@ ATbool ofp_traverse_FunType(ATerm term, pOFP_Traverse FunType)
    /** Traverse input (args) to production 
     */
    comma = "";
-   printf("   if (ATmatch(term, \"%s(", name);
+   // replace production name with the name given to the ATerm
+   printf("   if (ATmatch(term, \"%s(", (char*) FunType->pre);
    for (i = 0; i <  ATgetLength(args); i++) {
       printf("%s<term>", comma);
       comma = ",";
@@ -248,10 +359,11 @@ ATbool ofp_traverse_FunType(ATerm term, pOFP_Traverse FunType)
    for (i = 0; i <  ATgetLength(args); i++) {
       char * arg_name = "None";
       ATerm arg = ATelementAt(args, i);
-      ATbool list_type = ofp_isArgListType(arg);
+      ATbool list_type = ofp_isArgListKind(arg);
 
-      printf(", &%s", ofp_getArgName(arg, &arg_name));
+      printf(", &%s", ofp_getArgNameStr(arg, &arg_name));
       if (list_type) printf("_list");
+      else           printf("_term");
       printf(".term");
    }
    printf(")) {\n\n");
@@ -259,7 +371,31 @@ ATbool ofp_traverse_FunType(ATerm term, pOFP_Traverse FunType)
    /** Call traversal for input (arg) to production 
     */
    for (i = 0; i <  ATgetLength(args); i++) {
-      ofp_build_node_traversal(ATelementAt(args, i));
+      ATbool isOptType;
+      ATerm arg = ATelementAt(args, i);
+      ATerm type = ofp_getArgType(arg, &isOptType);
+      ATerm kind = ofp_getArgKind(arg);
+      ATerm name = ofp_getArgName(arg);
+      printf(".....arg....... %s\n", ATwriteToString(arg));
+      printf(".....type...... %s\n", ATwriteToString(type));
+      printf(".....kind...... %s\n", ATwriteToString(kind));
+      printf(".....name...... %s\n", ATwriteToString(name));
+
+      if (ofp_isTypeNameList(type)) {
+         int j;
+         ATermList nameList = (ATermList) type;
+         for (j = 0; j < ATgetLength(nameList); j++) {
+            arg = ATmake("[<term>,<term>]", kind, ATelementAt(nameList, j));
+            ofp_build_node_traversal(name, arg, kind);
+         }
+         // This is a production with or rules so one of the traversals must have
+         // matched and returned ATtrue. So return ATfalse in case none matched.
+         printf("      return ATfalse; /* for set of OR productions */\n");
+      }
+      else {
+         printf("building::::::::: %s\n", ATwriteToString(ofp_getArgName(arg)));
+         ofp_build_node_traversal(ofp_getArgName(arg), arg, kind);
+      }
    }
 
    printf("\n      return ATtrue;\n");
@@ -282,15 +418,12 @@ ATbool ofp_traverse_OpDecl(ATerm term, pOFP_Traverse OpDecl)
       char * String;
       if (ATmatch(Name.term, "<str>", &String)) {
          // MATCHED OpDecl name
+         FunType.pre = String;
       }
       if (ofp_traverse_FunType(FunType.term, &FunType)) {
          // MATCHED FunType
       } else return ATfalse;
 
-      return ATtrue;
-   }
-   else if (ATmatch(term, "OpDeclInj(<term>)", &FunType.term) ) {
-      // MATCHED OpDeclInj: already processed these terms
       return ATtrue;
    }
 
@@ -299,25 +432,91 @@ ATbool ofp_traverse_OpDecl(ATerm term, pOFP_Traverse OpDecl)
 
 ATbool ofp_traverse_OpDeclInj(ATerm term, pOFP_Traverse OpDeclInj)
 {
-   ATerm name, type;
+   ATerm name, type, opt;
+   int isOptType = 0;
+
    if (ATmatch(term, "OpDeclInj(<term>)", &OpDeclInj->term)) {
 #ifdef DEBUG_PRINT
       printf("\nofp_traverse_OpDeclInj: %s\n", ATwriteToString(OpDeclInj->term));
 #endif
       if (ATmatch(OpDeclInj->term, "FunType(<term>,<term>)", &type, &name)) {
-         if (ATmatch(type, "[ConstType(SortNoArgs(<term>))]", &type)) {
+         if (ATmatch(type, "[ConstType(Sort(<term>,<term>))]", &opt, &type)) {
+            // MATCHED option object type
+            if (ATmatch(type, "[SortNoArgs(<term>)]", &type) && ATisEqual(opt, ATmake("\"Option\""))) {
+               isOptType = 1;
+            } else return ATfalse;
+         }
+         else if (ATmatch(type, "[ConstType(SortNoArgs(<term>))]", &type)) {
             // MATCHED object type
-         } else return ATfalse;
+         }
          if (ATmatch(name, "ConstType(SortNoArgs(<term>))", &name)) {
             // MATCHED object name
          } else return ATfalse;
       } else return ATfalse;
 
-      OpDeclInj->term = ATmake("Type(<term>,[<term>])", name, type);
+      if (isOptType) {
+         OpDeclInj->term = ATmake("OptType(<term>,[<term>])", name, type);
+      }
+      else {
+         OpDeclInj->term = ATmake("Type(<term>,[<term>])", name, type);
+      }
 
       return ATtrue;
    }
    return ATfalse;
+}
+
+ATermList ofp_coalesceTypeTable(ATermList oldTable)
+{
+   // Assumes:
+   //  1. Contains list of terms Type(<str>,<list>) or OptType(<str>,<list>)
+   //      a. <str> is type name
+   //      b. <list> is [type] of length 1
+   //  2. Portions of table to be coalesced are in order
+   //  3. If OptType must match "(Some(<term>))"
+   //
+   ATerm head;
+   int isOptType;
+
+   ATermList table = (ATermList) ATmake("[]");
+   ATermList types = (ATermList) ATmake("[]");
+   ATermList tail  = (ATermList) ATmake("<term>", oldTable);
+
+   head = ATgetFirst(tail);
+   tail = ATgetNext(tail);
+
+   while (1) {
+      ATerm headName, headType, next, nextName, nextType;
+
+      if (ATisEmpty(tail)) next = ATmake("Type(None,[None])");
+      else                 next = ATgetFirst(tail);
+
+      if      ( ATmatch(head, "Type(<term>,[<term>])",    &headName, &headType) ) isOptType = 0;
+      else if ( ATmatch(head, "OptType(<term>,[<term>])", &headName, &headType) ) isOptType = 1;
+      else assert(0); 
+
+      assert(    ATmatch(next, "Type(<term>,[<term>])",    &nextName, &nextType)
+              || ATmatch(next, "OptType(<term>,[<term>])", &nextName, &nextType)
+            );
+
+      types = ATappend(types, headType);
+
+      // check for need to coalesce
+      if (! ATisEqual(headName, nextName)) {
+         if (isOptType) {
+            table = ATappend((ATermList)table, ATmake("OptType(<term>,<term>)", headName, types));
+         } else {
+            table = ATappend((ATermList)table, ATmake(   "Type(<term>,<term>)", headName, types));
+         }
+         types = (ATermList) ATmake("[]");
+         if (ATisEmpty(tail)) break;
+      }
+
+      head = ATgetFirst(tail);
+      tail = ATgetNext(tail);
+   }
+
+   return table;
 }
 
 ATbool ofp_traverse_Constructors(ATerm term, pOFP_Traverse Constructors)
@@ -339,10 +538,14 @@ ATbool ofp_traverse_Constructors(ATerm term, pOFP_Traverse Constructors)
          OpDeclInj_tail = ATgetNext(OpDeclInj_tail);
 
          if (ofp_traverse_OpDeclInj(OpDeclInj.term, &OpDeclInj)) {
-            // MATCHED OpDecl
+            // MATCHED OpDeclInj
             gTypeTable = ATappend(gTypeTable, OpDeclInj.term);
          }
       }
+
+      /* Coalesce the type table so there is one name per list of types
+       */
+      gTypeTable = ofp_coalesceTypeTable(gTypeTable);
 
       ATermList OpDecl_tail = (ATermList) ATmake("<term>", OpDecl_list.term);
       while (! ATisEmpty(OpDecl_tail)) {
@@ -352,7 +555,7 @@ ATbool ofp_traverse_Constructors(ATerm term, pOFP_Traverse Constructors)
 
          if (ofp_traverse_OpDecl(OpDecl.term, &OpDecl)) {
             // MATCHED OpDecl
-         } else return ATfalse;
+         }
       }
 
       printf("\nTYPES: %s\n", ATwriteToString((ATerm) gTypeTable));
