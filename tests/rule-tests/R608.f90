ba5900ae52a50dc15bbd50d71f5d1803c76bed5d
rasmussn <rasmus@uoregon.edu>
2013-08-22 20:46:42 -0700

    Fixed rule numbering.

--- tests/rule-tests/R608.f90
+++ tests/rule-tests/R608.f90
@@ -1,7 +1,7 @@
-!! R609 substring
+!! R608 substring
 !    is  parent-string ( substring-range )
 !
-! Tested separately are: R610 (parent-string) and R611 (substring-range).
+! Tested separately are: R609 (parent-string) and R610 (substring-range).
 !
 ! substring tested as part of a designator, which can be part of a primary, 
 ! so tested as a rhs to an assignment-stmt.
