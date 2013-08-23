9dc8f92a940ff758b2fdb137616c8a43261439a6
rasmussn <rasmus@uoregon.edu>
2013-08-23 09:52:58 -0700

    Initial version.

---
+++ tests/rule-tests/R624.f90
@@ -0,0 +1,14 @@
+!! R624 image-selector
+!    is lbracket cosubscript-list rbracket
+!
+!! R625 cosubscript
+!    is scalar-int-expr
+!
+a[3] = 1
+b = a[3]
+
+! NOTE 6.17
+A(:)[1,4] = 1
+b = A(:)[1,4]
+
+END
