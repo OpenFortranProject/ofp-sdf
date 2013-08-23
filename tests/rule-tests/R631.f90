93a6c2429539c51999bb9119394f5344688ad75f
rasmussn <rasmus@uoregon.edu>
2013-08-23 10:02:17 -0700

    Added coarray allocation.

--- tests/rule-tests/R631.f90
+++ tests/rule-tests/R631.f90
@@ -3,4 +3,10 @@
 !                       [ lbracket allocate-co-array-spec rbracket ]
 
 allocate (this, that, theother)
+
+! NOTE 618
+ALLOCATE(T[*])
+ALLOCATE(T%AAC(N))
+ALLOCATE(T[Q]%AAC(N))    ! MUST be executed on image Q to be correct
+
 end
