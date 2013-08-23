6a69f346d571acd7b2843d4b6308299157634e84
rasmussn <rasmus@uoregon.edu>
2013-08-23 16:51:27 -0700

    Initial version.

---
+++ tests/rule-tests/R511.f90
@@ -0,0 +1,12 @@
+!! R511 explicit-coshape-spec
+!    is [ [ lower-cobound : ] upper-cobound, ]... [ lower-cobound : ] *
+!
+real :: A1[*]
+real :: A2[3:*]
+real :: A3[7,*]
+real :: A4[2:7,5:*]
+real :: A5[5,5,5,*]
+
+end
+
+
