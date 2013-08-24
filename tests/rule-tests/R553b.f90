84b9218486c72f7dec2a5613a7755f2531ffa077
rasmussn <rasmus@uoregon.edu>
2013-08-24 09:23:28 -0700

    Added code from NOTE 5.35

--- tests/rule-tests/R553b.f90
+++ tests/rule-tests/R553b.f90
@@ -2,9 +2,12 @@
 !    is POINTER [ :: ] pointer-decl-list
 
 !TODO - fix Ambiguity in common-stmt
-!common /common_block_name/a
+common /common_block_name/a
 
 10 save :: i, j
 save k, /common_block_name/
+
+! From NOTE 5.35
+SAVE A, B, C, / BLOCKA /, D
 
 end
