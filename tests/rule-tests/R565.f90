0e1ba90ffedf0e8acf38620df40b7c1fd164a008
rasmussn <rasmus@uoregon.edu>
2013-08-24 08:41:15 -0700

    Moved R564.f90 to R565.f90

--- tests/rule-tests/R565.f90
+++ tests/rule-tests/R565.f90
@@ -1,11 +1,16 @@
-!! R564 equivalence-stmt
+!! R565 equivalence-stmt
 !    is EQUIVALENCE equivalence-set-list
 !
-!  equivalence-set
+!! R566 equivalence-set
 !    is ( equivalence-object , equivalence-object-list )
-
+!
 REAL, DIMENSION(10) :: B, Y, Z
+CHARACTER (LEN = 4) :: A, B
+CHARACTER (LEN = 3) :: C (2)
 
 10 EQUIVALENCE (a,b(3)), (x,y(2),z(4))
+
+! From NOTE 5.42
+EQUIVALENCE (A, C (1)), (B, C (2))
 
 end
