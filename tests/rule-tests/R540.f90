94d4490fa54a495ee0055f39a5b17a488a0f73d6
rasmussn <rasmus@uoregon.edu>
2013-08-24 12:32:31 -0700

    Added string data initialization.

--- tests/rule-tests/R540.f90
+++ tests/rule-tests/R540.f90
@@ -3,7 +3,14 @@
 !
 ! TODO- Fix AMBIGUITIES
 !
+CHARACTER (LEN = 10)  NAME
+INTEGER, DIMENSION (0:9) :: MILES
+
 data truth, beauty, ugly / 3.14159,2.81828,6 /
 data foo / 3 /
 data bar, this, that, theother / 2*3,2*7.6 /
+
+! From NOTE 5.29
+DATA NAME / "JOHN DOE" /, MILES / 10 * 0 /
+
 end
