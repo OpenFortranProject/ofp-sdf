c01a7646eb726d5b7945b010f5365fe834d2eb55
rasmussn <rasmus@uoregon.edu>
2013-08-24 09:51:00 -0700

    Initial version.

---
+++ tests/rule-tests/R538.f90
@@ -0,0 +1,9 @@
+!! R538 data-i-do-object
+!    is array-element
+!    or scalar-structure-component
+!    or data-implied-do
+!
+data ( truth(i),beauty(i), i=1,2,3) / 15*0. /
+data ( ugly(i),lies(i),this(i),that(i),theother(i), i=1,2,3) / 15*0. /
+data (( foo(j,i), i=1,j), j=1,5) / 15*0. /
+end
