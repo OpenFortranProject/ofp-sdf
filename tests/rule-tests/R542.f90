cc0753448d1c880b9f76c0064b44ebe58d0498a8
rasmussn <rasmus@uoregon.edu>
2013-08-24 12:44:42 -0700

    Initial version.

---
+++ tests/rule-tests/R542.f90
@@ -0,0 +1,14 @@
+!! R542 data-stmt-constant
+!    is scalar-constant
+!    or scalar-constant-subobject
+!    or signed-int-literal-constant
+!    or signed-real-literal-constant
+!    or null-init
+!    or initial-data-target
+!    or structure-constructor
+
+!TODO - fix major AMBIGUITY and add more tests.
+
+data i / +1 /, r / -1.0 /
+
+end
