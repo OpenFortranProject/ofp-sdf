b79f3e1a57ed2da32375313a727a666f23bf166c
rasmussn <rasmus@uoregon.edu>
2013-08-24 09:55:28 -0700

    Initial version.

---
+++ tests/rule-tests/R540.f90
@@ -0,0 +1,9 @@
+!! R540 data-stmt-value
+! [ data-stmt-repeat * ] data-stmt-constant
+!
+! TODO- Fix AMBIGUITIES
+!
+data truth, beauty, ugly / 3.14159,2.81828,6 /
+data foo / 3 /
+data bar, this, that, theother / 2*3,2*7.6 /
+end
