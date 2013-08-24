48204a486b62e22a20f31916d31622001fa311e4
rasmussn <rasmus@uoregon.edu>
2013-08-24 11:25:42 -0700

    Initial version.

---
+++ tests/rule-tests/R535.f90
@@ -0,0 +1,10 @@
+!! R535 data-stmt-set
+!    is ata-stmt-object -list / data-stmt-value -list /
+!
+
+data truth, beauty, ugly /3,14,15/
+data a, (z%d, (B(i,j) , j=1,i), i=1,100,foo()) /3*1.0/ 
+data myname /person(21,'john')/
+data a%b/jj/
+
+end
