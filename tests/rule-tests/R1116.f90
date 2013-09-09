f284a2c146b48856143a786bcd37032e852b0ed8
rasmussn <rasmus@uoregon.edu>
2013-09-09 11:50:50 -0700

    Initial version.

---
+++ tests/rule-tests/R1116.f90
@@ -0,0 +1,8 @@
+!! R1116 submodule
+!     is submodule-stmt
+!           [ specification-part ]
+!           [ module-subprogram-part ]
+!        end-submodule-stmt
+!
+SUBMODULE (parent) submod
+END SUBMODULE submod
