0a90c8286f8223e4080a616a0d9226d08eae73aa
rasmussn <rasmus@uoregon.edu>
2013-09-09 12:03:02 -0700

    Initial version.

---
+++ tests/rule-tests/R438.f90
@@ -0,0 +1,12 @@
+!! R438 component-decl
+!    is omponent-name
+!           [ ( component-array-spec ) ]
+!           [ lbracket coarray-spec rbracket ]
+!           [ * char-length ] [ component-initialization ]
+!
+! from NOTE 4.30
+   TYPE GRID_TYPE 
+      REAL, ALLOCATABLE, CODIMENSION[:,:,:] :: GRID1(:,:,:) 
+      real, ALLOCATABLE :: GRID2(:,:,:)[:,:,:]
+   END TYPE GRID_TYPE 
+end
