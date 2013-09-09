3d9e8f8612c23c7079d387e8109bbd5d706b132f
rasmussn <rasmus@uoregon.edu>
2013-09-09 12:32:44 -0700

    Initial version.

---
+++ tests/rule-tests/R818.f90
@@ -0,0 +1,11 @@
+!! R818 loop-control
+!    is [ , ] do-variable = scalar-int-expr, scalar-int-expr [ , scalar-int-expr ]
+!    or [ , ] WHILE ( scalar-logical-expr )
+!    or [ , ] CONCURRENT forall-header
+!
+! from NOTE 8.10
+   DO CONCURRENT (I = 1:N)
+      A(I) = B(I)
+   END DO
+
+end
