547f3285abc0ae5bde4e4bbc51a10d2c1023efd6
rasmussn <rasmus@uoregon.edu>
2013-10-01 11:47:46 -0700

    Cleaned up code.

--- fortran/traverse/build_lists.c
+++ fortran/traverse/build_lists.c
@@ -5,33 +5,17 @@
 
 #undef DEBUG_PRINT
 
-//---------------------------------------------------------------
-// Steps to traverse a production
-//    1. Get production name
-//    2. Get list of source terms
-//    3.  a. build source variable list
-//        b. match prodution term populating source variables
-//    4. For all source terms
-//        a. if source is a terminal, match source         -- FINISHED
-//        b. if source is not a terminal, traverse source  -- RECUR
-//        c. if source is optional, match, traverse source -- RECUR
-//        d. if source is a list, traverse all sources     -- RECUR
-//        e. if source is an alias?????
-//---------------------------------------------------------------
-
 // global variables
 //
 FILE * fph = NULL;
 FILE * fpc = NULL;
-
-ATermList gTypeAliases;  // TODO - delete me
 
 /**
  * Perform any necessary initialization for this traversal
  */
 ATbool ofp_traverse_init()
 {
-   fpc = fopen("ofp_list_builder.c", "w");    assert(fpc != NULL);
+   fpc = fopen("ofp_traverse_lists.c", "w");    assert(fpc != NULL);
    fprintf(fpc, "#include \"traversal.h\"\n\n");
 
    return ATtrue;
@@ -100,18 +84,6 @@ ATbool ofp_build_list_traversal(ATerm term, ATerm name)
    }
 
    return ATfalse;
-
-#ifdef NOT_ME
-      OFP_Traverse EntityDecl;
-      ATermList EntityDecl_tail = (ATermList) ATmake("<term>", EntityDecl_list.term);
-      while (! ATisEmpty(EntityDecl_tail)) {
-         EntityDecl.term = ATgetFirst(EntityDecl_tail);
-         EntityDecl_tail = ATgetNext(EntityDecl_tail);
-         if (ofp_traverse_EntityDecl(EntityDecl.term, &EntityDecl)) {
-            // MATCHED EntityDecl
-         } else return ATfalse;
-      }
-#endif
 }
 
 ATbool ofp_traverse_OpDecl(ATerm term, pOFP_Traverse OpDecl)
@@ -145,31 +117,6 @@ ATbool ofp_traverse_OpDecl(ATerm term, pOFP_Traverse OpDecl)
 
 ATbool ofp_traverse_OpDeclInj(ATerm term, pOFP_Traverse OpDeclInj)
 {
-   ATerm alias, type, opt;
-   int isOptType = 0;
-
-   if (ATmatch(term, "OpDeclInj(<term>)", &OpDeclInj->term)) {
-#ifdef DEBUG_PRINT
-      printf("\nofp_traverse_OpDeclInj: %s\n", ATwriteToString(OpDeclInj->term));
-#endif
-      if (ATmatch(OpDeclInj->term, "FunType(<term>,<term>)", &type, &alias)) {
-         ATermList list;
-         if (ATmatch(type, "<term>", &list)) {
-            // not a simple alias
-            if (ATgetLength(list) > 1) return ATfalse;
-         } else return ATfalse;
-         if (ATmatch(type, "[ConstType(SortNoArgs(<term>))]", &type)) {
-            // MATCHED object type
-         } else return ATfalse;
-         if (ATmatch(alias, "ConstType(SortNoArgs(<term>))", &alias)) {
-            // MATCHED object alias
-         } else return ATfalse;
-      } else return ATfalse;
-
-      OpDeclInj->term = ATmake("Alias(<term>,<term>)", type, alias);
-
-      return ATtrue;
-   }
    return ATfalse;
 }
 
@@ -181,25 +128,6 @@ ATbool ofp_traverse_Constructors(ATerm term, pOFP_Traverse Constructors)
 
    OFP_Traverse OpDecl_list;
    if (ATmatch(term, "Constructors(<term>)", &OpDecl_list.term) ) {
-      gTypeAliases = (ATermList) ATmake("[]");
-
-      /* First build the type aliase table.  It is needed when matching productions.
-       */
-      ATermList OpDeclInj_tail = (ATermList) ATmake("<term>", OpDecl_list.term);
-      while (! ATisEmpty(OpDeclInj_tail)) {
-         OFP_Traverse OpDeclInj;
-         OpDeclInj.term = ATgetFirst(OpDeclInj_tail);
-         OpDeclInj_tail = ATgetNext(OpDeclInj_tail);
-
-         if (ofp_traverse_OpDeclInj(OpDeclInj.term, &OpDeclInj)) {
-            // MATCHED OpDeclInj
-            gTypeAliases = ATappend(gTypeAliases, OpDeclInj.term);
-         }
-      }
-
-      /* Coalesce the type table so there is one name per list of types
-       */
-      gTypeAliases = ofp_coalesceAliasTable(gTypeAliases);
 
       ATermList OpDecl_tail = (ATermList) ATmake("<term>", OpDecl_list.term);
       while (! ATisEmpty(OpDecl_tail)) {
@@ -211,8 +139,6 @@ ATbool ofp_traverse_Constructors(ATerm term, pOFP_Traverse Constructors)
             // MATCHED OpDecl
          }
       }
-
-      printf("\nALIASES: %s\n", ATwriteToString((ATerm) gTypeAliases));
 
       return ATtrue;
    }
