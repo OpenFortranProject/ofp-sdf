f1dc36da78e1ecac58869b65f65f5ab72aa259e2
rasmussn <rasmus@uoregon.edu>
2013-10-12 16:41:15 -0700

    Moved some functions to ofp_builder.c

--- fortran/traverse/build_string_traversals.c
+++ fortran/traverse/build_string_traversals.c
@@ -1,15 +1,9 @@
-#include "traversal.h"
+#include "ofp_builder.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
 #undef DEBUG_PRINT
-
-// global variables
-//
-FILE * fpc = NULL;
-
-ATermList gTypeAliases;  // TODO - delete me
 
 /**
  * Perform any necessary initialization for this traversal
@@ -28,74 +22,6 @@ ATbool ofp_traverse_finalize()
 {
    fclose(fpc);
    return ATtrue;
-}
-
-ATbool ofp_build_traversal_func_begin(ATerm name)
-{
-   char * percs = "%s";
-   char * nameStr;
-
-   if (! ATmatch(name, "<str>", &nameStr)) {
-      return ATfalse;
-   }
-
-   /** write to implementation file
-    */
-   fprintf(fpc, "ATbool ofp_traverse_%s(ATerm term, pOFP_Traverse %s)\n", nameStr, nameStr);
-   fprintf(fpc, "{\n");
-
-   /** Debugging output
-    */
-   fprintf(fpc, "#ifdef DEBUG_PRINT\n");
-   fprintf(fpc, "   printf(\"%s: %s\\n\", ATwriteToString(term));\n", nameStr, percs);
-   fprintf(fpc, "#endif\n\n");
-
-   return ATtrue;
-}
-
-ATbool ofp_build_traversal_func_end(ATerm name)
-{
-   fprintf(fpc, "   return ATtrue;\n");
-   fprintf(fpc, "}\n\n");
-   return ATtrue;
-}
-
-ATbool ofp_build_traversal_string_match(ATerm name)
-{
-   char * nameStr;
-
-   if (! ATmatch(name, "<str>", &nameStr)) {
-      return ATfalse;
-   }
-
-   /** Match the name term, otherwise return false
-    */
-   fprintf(fpc, "   char * %s_val;\n", nameStr);
-   fprintf(fpc, "   if (ATmatch(term, \"<str>\", &%s_val)) {\n", nameStr);
-   fprintf(fpc, "      // MATCHED %s\n", nameStr);
-   fprintf(fpc, "      return ATtrue;\n");
-   fprintf(fpc, "   } else return ATfalse;\n\n");
-
-   return ATtrue;
-}
-
-ATbool ofp_build_string_traversal(ATerm type, ATerm name)
-{
-   char * nameStr;
-
-   if (! ATisEqual(type, ATmake("<str>", "String"))) {
-      return ATfalse;
-   }
-
-   if (! ATmatch(name, "<str>", &nameStr)) {
-      return ATfalse;
-   }
-
-   ofp_build_traversal_func_begin(name);
-   ofp_build_traversal_string_match(name);
-   ofp_build_traversal_func_end(name);
-
-   return ATfalse;
 }
 
 ATbool ofp_traverse_OpDecl(ATerm term, pOFP_Traverse OpDecl)
