3f93d2e68927179d0d41cf5f2f7ff96d71c048c5
rasmussn <rasmus@uoregon.edu>
2013-11-18 19:11:08 -0800

    Moved traversals of terminals from ofp_traverse_strings.c to this file and removed dead/obsolete code.

--- fortran/traverse/traversal.c
+++ fortran/traverse/traversal.c
@@ -1,5 +1,4 @@
 #include "traversal.h"
-#include <assert.h>
 
 #define DEBUG_PRINT
 
@@ -11,67 +10,198 @@ ATbool ofp_traverse_init()
    return ATtrue;
 }
 
-/**
- * Section/Clause 4: Types
- */
-
-
-/**
- * Section/Clause 5: Attribute declarations and specifications
- */
-
-
-//========================================================================================
-// R722 expr
-//----------------------------------------------------------------------------------------
-ATbool ofp_traverse_Expr(ATerm term, pOFP_Traverse Expr)
+ATbool ofp_traverse_Dop(ATerm term, pOFP_Traverse Dop)
 {
 #ifdef DEBUG_PRINT
-   printf("\nExpr: %s\n", ATwriteToString(term));
+   printf("Dop: %s\n", ATwriteToString(term));
 #endif
 
-   OFP_Traverse IntLiteralConstant;
-   if (ATmatch(term, "Expr(<term>)", &IntLiteralConstant.term)) {
-      if (ofp_traverse_IntLiteralConstant(IntLiteralConstant.term, &IntLiteralConstant)) {
-         // MATCHED IntLiteralConstant
-      } else return ATfalse;
-
+   char * Dop_val;
+   if (ATmatch(term, "<str>", &Dop_val)) {
+      // MATCHED Dop
       return ATtrue;
-   }
+   } else return ATfalse;
 
-   return ATfalse;
+   return ATtrue;
 }
 
-//========================================================================================
-// R732 assignment-stmt
-//----------------------------------------------------------------------------------------
-ATbool ofp_traverse_AssignmentStmt(ATerm term, pOFP_Traverse AssignmentStmt)
+ATbool ofp_traverse_HexConstant(ATerm term, pOFP_Traverse HexConstant)
 {
 #ifdef DEBUG_PRINT
-   printf("\nAssignmentStmt: %s\n", ATwriteToString(term));
+   printf("HexConstant: %s\n", ATwriteToString(term));
 #endif
 
-   OFP_Traverse Label, Variable, Expr, EOS;
-   if (ATmatch(term, "AssignmentStmt(<term>,<term>,<term>,<term>)", &Label.term, &Variable.term, &Expr.term, &EOS.term)) {
-      if (ATmatch(Label.term, "Some(<term>)", &Label.term)) {
-         if (ofp_traverse_Label(Label.term, &Label)) {
-            // MATCHED Label
-         } else return ATfalse;
-      }
-
-      if (ofp_traverse_Variable(Variable.term, &Variable)) {
-         // MATCHED Variable
-      } else return ATfalse;
-
-      if (ofp_traverse_Expr(Expr.term, &Expr)) {
-         // MATCHED Expr
-      } else return ATfalse;
-
-      if (ofp_traverse_EOS(EOS.term, &EOS)) {
-         // MATCHED EOS
-      } else return ATfalse;
-
+   char * HexConstant_val;
+   if (ATmatch(term, "<str>", &HexConstant_val)) {
+      // MATCHED HexConstant
       return ATtrue;
-   }
-   return ATfalse;
+   } else return ATfalse;
+
+   return ATtrue;
 }
+
+ATbool ofp_traverse_OctalConstant(ATerm term, pOFP_Traverse OctalConstant)
+{
+#ifdef DEBUG_PRINT
+   printf("OctalConstant: %s\n", ATwriteToString(term));
+#endif
+
+   char * OctalConstant_val;
+   if (ATmatch(term, "<str>", &OctalConstant_val)) {
+      // MATCHED OctalConstant
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_BinaryConstant(ATerm term, pOFP_Traverse BinaryConstant)
+{
+#ifdef DEBUG_PRINT
+   printf("BinaryConstant: %s\n", ATwriteToString(term));
+#endif
+
+   char * BinaryConstant_val;
+   if (ATmatch(term, "<str>", &BinaryConstant_val)) {
+      // MATCHED BinaryConstant
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Rcon(ATerm term, pOFP_Traverse Rcon)
+{
+#ifdef DEBUG_PRINT
+   printf("Rcon: %s\n", ATwriteToString(term));
+#endif
+
+   char * Rcon_val;
+   if (ATmatch(term, "<str>", &Rcon_val)) {
+      // MATCHED Rcon
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Scon(ATerm term, pOFP_Traverse Scon)
+{
+#ifdef DEBUG_PRINT
+   printf("Scon: %s\n", ATwriteToString(term));
+#endif
+
+   char * Scon_val;
+   if (ATmatch(term, "<str>", &Scon_val)) {
+      // MATCHED Scon
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Icon(ATerm term, pOFP_Traverse Icon)
+{
+#ifdef DEBUG_PRINT
+   printf("Icon: %s\n", ATwriteToString(term));
+#endif
+
+   char * Icon_val;
+   if (ATmatch(term, "<str>", &Icon_val)) {
+      // MATCHED Icon
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Ident(ATerm term, pOFP_Traverse Ident)
+{
+#ifdef DEBUG_PRINT
+   printf("Ident: %s\n", ATwriteToString(term));
+#endif
+
+   char * Ident_val;
+   if (ATmatch(term, "<str>", &Ident_val)) {
+      // MATCHED Ident
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Letter(ATerm term, pOFP_Traverse Letter)
+{
+#ifdef DEBUG_PRINT
+   printf("Letter: %s\n", ATwriteToString(term));
+#endif
+
+   char * Letter_val;
+   if (ATmatch(term, "<str>", &Letter_val)) {
+      // MATCHED Letter
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_Label(ATerm term, pOFP_Traverse Label)
+{
+#ifdef DEBUG_PRINT
+   printf("Label: %s\n", ATwriteToString(term));
+#endif
+
+   char * Label_val;
+   if (ATmatch(term, "<str>", &Label_val)) {
+      // MATCHED Label
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_LblRef(ATerm term, pOFP_Traverse LblRef)
+{
+#ifdef DEBUG_PRINT
+   printf("LblRef: %s\n", ATwriteToString(term));
+#endif
+
+   char * LblRef_val;
+   if (ATmatch(term, "<str>", &LblRef_val)) {
+      // MATCHED LblRef
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_StartCommentBlock(ATerm term, pOFP_Traverse StartCommentBlock)
+{
+#ifdef DEBUG_PRINT
+   printf("StartCommentBlock: %s\n", ATwriteToString(term));
+#endif
+
+   char * StartCommentBlock_val;
+   if (ATmatch(term, "<str>", &StartCommentBlock_val)) {
+      // MATCHED StartCommentBlock
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
+ATbool ofp_traverse_EOS(ATerm term, pOFP_Traverse EOS)
+{
+#ifdef DEBUG_PRINT
+   printf("EOS: %s\n", ATwriteToString(term));
+#endif
+
+   char * EOS_val;
+   if (ATmatch(term, "<str>", &EOS_val)) {
+      // MATCHED EOS
+      return ATtrue;
+   } else return ATfalse;
+
+   return ATtrue;
+}
+
