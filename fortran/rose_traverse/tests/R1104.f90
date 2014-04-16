dddacbffd6d1cb061873c1a7f1cc1f99658e16f6
rasmussn <rasmus@uoregon.edu>
2014-04-15 20:38:23 -0700

    Initial version.

---
+++ fortran/rose_traverse/tests/R1104.f90
@@ -0,0 +1,66 @@
+! Test module
+!      module  is  module-stmt
+!                    [ specification-part ]
+!                    [ module-subprogram-part ]
+!                  end-module-stmt
+!
+!      module-stmt  is  MODULE module-name
+!
+!      end-module-stmt  is  END [ MODULE [ module-name ] ]
+!
+!      module-subprogram-part  is  contains-stmt 
+!                                    module-subprogram
+!                                    [ module-subprogram ] ...
+!
+!      module-subprogram  is  function-subprogram 
+!                         or  subroutine-subprogram
+!
+! Not tested here: specification-part, function-subprogram, and 
+! subroutine-subprogram.
+
+! None of the optional parts included
+MODULE a
+END
+
+! Include the optional MODULE in end-module-stmt
+MODULE b
+END MODULE
+
+! Include optional MODULE and module-name in end-module-stmt.
+MODULE c
+END MODULE c
+
+! Include an optional specification-part
+MODULE d
+  Integer i
+END MODULE d
+
+! Include an optional module-subprogram-part
+MODULE e
+CONTAINS
+  subroutine sub()
+  END subroutine sub
+  Function foo()
+    foo = 13
+  END FUNCTION foo
+END MODULE e
+
+! Include an optional separate-module-subprogram
+MODULE f
+CONTAINS
+  MODULE procedure mp
+  end PROCEDURE mp
+END MODULE f
+
+! Include all optional parts
+MODULE g
+  integer i
+contains
+  subroutine sub()
+  END subroutine sub
+  FUNCTION foo()
+    foo = 13
+  END FUNCTION foo
+  module PROCEDURE mp
+  END procedure mp
+END MODULE g
