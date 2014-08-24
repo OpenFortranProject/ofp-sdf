f178b02eafc345d341e9ca6281f3b4dd44300357
rasmussn <rasmus@uoregon.edu>
2014-08-23 21:22:22 -0700

    Uncommented two ProcedureDeclarationStmts.

--- tests/rule-tests/R1214.f90
+++ tests/rule-tests/R1214.f90
@@ -2,7 +2,7 @@
 !       is procedure-entity-name [ => proc-pointer-init ]
 
 ! Testing proc decl list.
-!TODO-F08 PROCEDURE () foo
-!TODO-F08 PROCEDURE () truth, beauty, ugly, lies
+PROCEDURE () foo
+PROCEDURE () truth, beauty, ugly, lies
 !TODO-F08 proc-pointer-init
 END
