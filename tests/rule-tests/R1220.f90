6aecf987a76c09bceb3fd79b9a1aa7bb872d68b9
Craig Rasmussen <rasmus@uoregon.edu>
2014-08-25 17:19:40 -0700

    Added a real constant to an arg and added different 'keyword' names.

--- tests/rule-tests/R1220.f90
+++ tests/rule-tests/R1220.f90
@@ -4,11 +4,11 @@
 !
 CALL foo1
 CALL foo2()
-CALL foo3(1)
+CALL foo3(1.)
 CALL foo4(1,2)
 CALL foo5(a)
-CALL foo6(keyword=.not.B)
-CALL foo7(keyword = b)
-CALL foo8(keyword = udt%bar)
+CALL foo6(key1=.not.B)
+CALL foo7(key2 = b)
+CALL foo8(key3 = udt%bar)
 CALL foo9(keyword=*10)
 END
