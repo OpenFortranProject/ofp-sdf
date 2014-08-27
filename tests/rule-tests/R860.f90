635b71d2ed3c17d8571c08adfce734b3280e3dac
rasmussn <rasmus@uoregon.edu>
2014-08-26 17:37:13 -0700

    Added sync-stat-list.

--- tests/rule-tests/R860.f90
+++ tests/rule-tests/R860.f90
@@ -1,6 +1,10 @@
 !! R860 sync-images-stmt
 !    is SYNC IMAGES ( image-set [ , sync-stat-list ] )
 !
+character :: msg
+integer :: i
+
    SYNC IMAGES (1)
    sync images (*)
+ 1 sync IMAGes (i, stat=i, ERRmsg = msg)
 end
