e8c210b3ad5a6fa5d0382ce5be8ff0ef829a38a3
rasmussn <rasmus@uoregon.edu>
2014-08-31 18:22:40 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R553b.f90
@@ -0,0 +1,12 @@
+!! R553 save-stmt
+!    is POINTER [ :: ] pointer-decl-list
+
+common /common_block_name/a
+
+10 save :: i, j
+save k, /common_block_name/
+
+! From NOTE 5.35
+SAVE A, B, C, / BLOCKA /, D
+
+end
