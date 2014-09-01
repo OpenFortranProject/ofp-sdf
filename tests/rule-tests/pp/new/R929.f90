1b6d91871ab2493e4b37c389d128e13ee084b745
rasmussn <rasmus@uoregon.edu>
2014-09-01 10:00:04 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R929.f90
@@ -0,0 +1,16 @@
+!! R929 inquire-stmt
+!    is  INQUIRE ( inquire-spec-list )
+!    or  INQUIRE ( IOLENGTH = scalar-int-variable ) output-item-list
+!
+! Not tested here: inquire-spec-list, scalar-int-variable, and
+!  output-item-list.
+
+character :: cvar
+
+inquire(10)
+inquire(file='foo.txt', blank=cvar)
+inquire(10, blank=cvar)
+inquire(ENCODING=cvar, unit=10)
+inquire(iolength=ix) a, b, c
+
+end
