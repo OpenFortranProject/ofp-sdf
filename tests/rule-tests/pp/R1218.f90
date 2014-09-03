5cafc12f7e8fe10ae89c9c351359af796b7fbead
rasmussn <rasmus@uoregon.edu>
2014-09-02 20:24:48 -0700

    Initial version.

--- tests/rule-tests/pp/R1218.f90
+++ tests/rule-tests/pp/R1218.f90
@@ -1,9 +1,5 @@
-!! R1218 intrinsic-stmt
-!    is INTRINSIC [ :: ] intrinsic-procedure-name-list
-
-INTRINSIC sin
-INTRINSIC exp, mod
+INTRINSIC :: sin
+INTRINSIC :: exp, mod
 INTRINSIC :: cos
 INTRINSIC :: tan, atan
-
-END
+END PROGRAM
