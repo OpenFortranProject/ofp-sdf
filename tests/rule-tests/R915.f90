ca4374619e34b0ae68a958f34b20449c8f6bb814
Craig Rasmussen <rasmus@uoregon.edu>
2014-09-03 14:11:12 -0700

    Added io-implied-do input item.

--- tests/rule-tests/R915.f90
+++ tests/rule-tests/R915.f90
@@ -1,9 +1,10 @@
 !! R915 input-item
 !    is variable
 !    or io-implied-do
-
-!TODO-F08 add test of io-implied-do
-
+!
 read *, truth, beauty
 read *, or, ugly, lies 
+
+9 read (1, *) (A, i=1,10,2)
+
 end
