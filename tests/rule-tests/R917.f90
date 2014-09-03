200ac122677e0d4c4617f6ed1ada0614d5483d26
Craig Rasmussen <rasmus@uoregon.edu>
2014-09-03 14:23:59 -0700

    Added io-implied-do and an expr as an output-item.

--- tests/rule-tests/R917.f90
+++ tests/rule-tests/R917.f90
@@ -1,9 +1,10 @@
-!! R916 output-item
+!! R917 output-item
 !    is expr
 !    or io-implied-do
-
-!TODO-F08 - add testing of io-implied-do
-
+!
 write (*,*) truth, beauty
-write (*,*) or, ugly, lies 
+write (*,*) or, ugly, 1+3
+
+write (1) (a, i=1,2,3)
+
 end
