309e394d12583313ce8378c0023ae93b333a7ccc
rasmussn <rasmus@uoregon.edu>
2014-09-02 20:59:24 -0700

    Changed to be what pp should produce.

--- tests/rule-tests/pp/R926.f90
+++ tests/rule-tests/pp/R926.f90
@@ -1,12 +1,4 @@
-!! R926 position-spec
-!    is  [ UNIT = ] file-unit-number
-!    or  IOMSG = iomsg-variable
-!    or  IOSTAT = scalar-int-variable
-!    or  ERR = label
-
-character cvar
-
-backspace (iomsg=CVAR, unit=5)
-rewind (5, iomsg=cvar,err=314,iostat=iugly)
-
-314 end
+CHARACTER :: cvar
+BACKSPACE (IOMSG=CVAR, UNIT=5)
+REWIND (UNIT=5, IOMSG=cvar, ERR=314, IOSTAT=iugly)
+314 END PROGRAM
