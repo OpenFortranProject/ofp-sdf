cdc8ab4a0bd78f72ad83ecbf22e3a40edcaa6fc0
rasmussn <rasmus@uoregon.edu>
2014-09-03 20:06:14 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R832.f90
@@ -0,0 +1,41 @@
+!! R832 if-construct
+!    is  if-then-stmt
+!              block
+!          [ else-if-smt 
+!              block ] ...
+!          [ else-stmt 
+!              block ]
+!         end-if-stmt
+!
+! Also tests rules R841 (if-then-stmt), R842 (else-if-stmt), R843 (else-stmt), 
+! R844 (end-if-stmt), and if-construct-name.  Does not test 
+! scalar-logical-expr.
+
+10 if(x .eq. y) then
+   x = 1
+11 else if(x .lt. y) then
+   x = 0
+12 else
+   x = -1
+13 end if
+
+if(x .eq. y) then
+   x = 1
+else
+   x = -1
+end if
+
+if(x .eq. y) then
+   x = 1
+end if
+
+my_if: if(x .eq. y) then
+   x = 1
+else if(x .lt. y) then my_if
+   x = 0
+else my_if
+   x = -1
+end if my_if
+
+end
+
