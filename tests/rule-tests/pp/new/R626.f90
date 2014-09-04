289372cdda86c04e0d01c8bc62c50bc7092bc861
rasmussn <rasmus@uoregon.edu>
2014-09-03 20:16:09 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R626.f90
@@ -0,0 +1,14 @@
+!! R626 allocate-stmt
+!    is  ALLOCATE ( [ type-spec :: ] allocation-list [, alloc-opt-list ] )
+!
+! Tested separately are: type-spec (R402) and allocation-list (R631).
+
+10 ALLOCATE(a,b)
+allocate(integer::a,b)
+allocate(a,stat=i, errmsg=blah, source=k)
+
+! NOTE 6.19
+ALLOCATE (X (N), B (-3 : M, 0:9), STAT = IERR_ALLOC)
+
+end
+
