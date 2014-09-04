0caa1570e7d4a35715e71febbbbf52dfdb0d380a
rasmussn <rasmus@uoregon.edu>
2014-09-03 19:02:14 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R846.f90
@@ -0,0 +1,48 @@
+!! R846 select-type-construct
+!      select-type-construct  is  select-type-stmt
+!                                   [ type-guard-stmt 
+!                                       block ] ...
+!                                 end-select-type-stmt
+!
+!      select-type-stmt  is  [ select-construct-name : ] SELECT TYPE 
+!                              ( [ associate-name => ] selector)
+!
+!      type-guard-stmt  is  TYPE IS ( type-spec) [ select-construct-name ]
+!                       or  CLASS IS ( type-spec ) [ select-construct-name ]
+!                       or  CLASS DEFAULT [ select-construct-name ]
+!
+!      end-select-type-stmt  is  END SELECT [ select-construct-name ]
+!
+! Tested here: select-type-construct, select-type-stmt, type-guard-stmt, and 
+! end-select-type-stmt.
+!
+! Not tested here: block, select-construct-name, associate-name, selector, 
+! and type-spec
+10 select type (a => p)
+11 class is ( point )
+   x = 1
+12 type is (point_3d)
+   x = 2
+13 class default
+   x = 3
+14 end select
+
+select type (p)
+class is ( point )
+   x = 1
+type is (point_3d)
+   x = 2
+class default
+   x = 3
+end select
+
+my_select: select type (p)
+class is ( point ) my_select
+   x = 1
+type is (point_3d) my_select
+   x = 2
+class default my_select
+   x = 3
+end select my_select
+
+end
