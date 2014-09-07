34b964b6a143fbdb128ae81b6e6d77b102668f67
rasmussn <rasmus@uoregon.edu>
2014-09-07 13:59:20 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R448.f90
@@ -0,0 +1,15 @@
+!! R448 type-bound-procedure-stmt
+!    is PROCEDURE [ [ , binding-attr-list ] :: ] type-bound-proc-decl-list
+!    or PROCEDURE (interface-name ), binding-attr-list :: binding-name-list
+
+module myjunk
+
+TYPE POINT
+CONTAINS
+   PROCEDURE, PASS :: LENGTH => POINT_LENGTH
+   procedure length => point_length
+   procedure :: length => point_length
+10 procedure (proc_name), non_overridable, DEFERRED :: sink, or, swim
+END TYPE POINT
+
+end module
