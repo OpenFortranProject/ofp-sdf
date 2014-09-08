8684536f33c0e0027f53459342926e51e751d4e9
rasmussn <rasmus@uoregon.edu>
2014-09-07 22:29:39 -0700

    Initial version.

---
+++ tests/rule-tests/R440.f90
@@ -0,0 +1,13 @@
+!! R440 proc-component-def-stmt
+!    is PROCEDURE ( [ proc-interface ] ) ,
+!            proc-component-attr-spec-list :: proc-decl-list
+!                                                                                         
+
+type feeble
+
+  procedure (), pointer :: foible
+  procedure (bob), pass, private :: foo => bar
+
+end type
+
+end
