c8cfab928cfa843928b91c1f7bac100d25c6d3ff
rasmussn <rasmus@uoregon.edu>
2014-09-07 23:15:12 -0700

    Initial version.

---
+++ tests/rule-tests/pp/new/R426.f90
@@ -0,0 +1,12 @@
+!! R426 derived-type-stmt
+!    is TYPE [ [ , type-attr-spec-list ] :: ] type-name [ ( type-param-name-list ) ]
+
+module DTS
+
+10 TYPE binky
+end type
+
+type, abstract, private :: boopy(l,m)
+end type boopy
+
+end module
