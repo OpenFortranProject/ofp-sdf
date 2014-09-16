85391e6799c56ede03783bc32b68f5c4c5109e1b
rasmussn <rasmus@uoregon.edu>
2014-09-15 21:35:33 -0700

    Removed include of rose.h

--- fortran/rose_traverse/OFPNodes.hpp
+++ fortran/rose_traverse/OFPNodes.hpp
@@ -1,11 +1,9 @@
 #ifndef	OFP_NODES_H
 #define	OFP_NODES_H
 
-#define COMPILED_WITH_ROSE
+#define COMPILED_WITH_ROSE 0
 
-#ifdef  COMPILED_WITH_ROSE
-#include "rose.h"
-#else
+#if COMPILED_WITH_ROSE==0
 #include "UntypedNodes.h"
 #endif
 
