f240fb6252335b30b5d266012715f6d77f8d70cb
rasmussn <rasmus@uoregon.edu>
2014-09-18 19:11:35 -0700

    Finished so moved upstairs.

--- tests/rule-tests/pp/new/R524.f90
+++
@@ -1,9 +0,0 @@
-!! R524 access-stmt
-!    is access-spec [ [ :: ] access-id-list ]
-
-module ex
-10  private
-    PUBLIC boo, assignment(=)
-    public :: binky, bonky, operator (+)
-
-end module ex
