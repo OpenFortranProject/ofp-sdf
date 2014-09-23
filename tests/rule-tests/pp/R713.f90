7c7fc9ff3be68c957b2f4f8c380accce9ca53021
Craig Rasmussen <rasmus@uoregon.edu>
2014-09-23 10:04:05 -0700

    Initial version.

---
+++ tests/rule-tests/pp/R713.f90
@@ -0,0 +1,14 @@
+LOGICAL :: a
+a = 1 .EQ. 2
+a = 1 .EQ. 2
+a = 1 .NE. 2
+a = 1 .NE. 2
+a = 1 .LT. 2
+a = 1 .LT. 2
+a = 1 .LE. 2
+a = 1 .LE. 2
+a = 1 .GT. 2
+a = 1 .GT. 2
+a = 1 .GE. 2
+a = 1 .GE. 2
+END PROGRAM
