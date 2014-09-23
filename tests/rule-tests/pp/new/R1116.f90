b66c97cb33168b734218b74ef4947660c6faf381
Craig Rasmussen <rasmus@uoregon.edu>
2014-09-23 10:00:09 -0700

    Moved upstairs.

--- tests/rule-tests/pp/new/R1116.f90
+++
@@ -1,8 +0,0 @@
-!! R1116 submodule
-!     is submodule-stmt
-!           [ specification-part ]
-!           [ module-subprogram-part ]
-!        end-submodule-stmt
-!
-SUBMODULE (parent) submod
-END SUBMODULE submod
