30bac1aed9bfd7154b446998c6161035b0b73978
rasmussn <rasmus@uoregon.edu>
2014-09-24 21:50:11 -0700

    Added declarations to make compilable.

--- tests/rule-tests/R611.f90
+++ tests/rule-tests/R611.f90
@@ -1,8 +1,19 @@
 !! R611 data-ref
 !    is part-ref [ % part-ref ] ...
 !
-lhs = a
-lhs = a%b
-lhs = a%b%c
+type TB
+  integer :: c
+end type TB
+type TA
+  type(TB) :: b
+end type TA
+
+type(TA) :: lhs_a, a
+type(TB) :: lhs_b
+integer  :: lhs_c
+
+lhs_a = a
+lhs_b = a%b
+lhs_c = a%b%c
 
 end
