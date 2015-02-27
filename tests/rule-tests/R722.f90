0a63509a0c845396e5eacb677ccfb91e03d95198
rasmussn <rasmus@uoregon.edu>
2015-02-27 13:38:53 -0800

    Moved towards being able to compile.

--- tests/rule-tests/R722.f90
+++ tests/rule-tests/R722.f90
@@ -1,12 +1,16 @@
 !! R722 expr
 !    is [ expr defined-binary-op ] level-5-expr
+CHARACTER(LEN=10) :: H
+LOGICAL :: a
+INTEGER :: i
+TYPE(set) :: s
 
-a = (1 .intersect. 2) .UNION. (3 - 4)
+s = (1 .intersect. 2) .UNION. (3 - 4)
 a = 1 + 2 == 2 * 4
-a = .inverse. (1 + 2)
-a = 1 + 2 .and. 3 * 4
+s = .inverse. (1 + 2)
+a = ((1+2) .EQ. 3) .AND. ((3*4) .EQ. 12)
 a = "E" // "G" == H (1:10)
-a = 1 + 2 * 3
-a = (1 + 2) * 3
+i = 1 + 2 * 3
+i = (1 + 2) * 3
 
 end
