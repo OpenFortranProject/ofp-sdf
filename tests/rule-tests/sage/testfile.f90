28c8edbb7e149d6da471df566c9873459841857d
rasmussn <rasmus@uoregon.edu>
2015-08-19 11:15:28 -0700

    Initial version.

---
+++ tests/rule-tests/sage/testfile.f90
@@ -0,0 +1,11 @@
+10 program main
+      implicit none
+      integer :: a,b
+      logical :: f
+      a = 1
+      a = b - 1
+      a = b * 1
+      a = b / 1.3
+      f = b < 1_4
+20 end program main
+
