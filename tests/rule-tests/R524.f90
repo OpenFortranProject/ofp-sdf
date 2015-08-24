02fd87d8efb9ae0d1b96998dd0cd0848509f19e9
rasmussn <rasmus@uoregon.edu>
2015-08-24 08:44:34 -0700

    Fixed for compilation.

--- tests/rule-tests/R524.f90
+++ tests/rule-tests/R524.f90
@@ -6,4 +6,18 @@ module ex
     PUBLIC boo, assignment(=)
     public :: binky, bonky, operator (+)
 
+INTERFACE ASSIGNMENT ( = )
+   SUBROUTINE LOGICAL_TO_NUMERIC (N, B)
+      INTEGER, INTENT (OUT) :: N
+      LOGICAL, INTENT (IN)  :: B
+   END SUBROUTINE LOGICAL_TO_NUMERIC
+END INTERFACE ASSIGNMENT ( = )
+
+INTERFACE OPERATOR ( + )
+   LOGICAL FUNCTION LOGICAL_ADD (N, B)
+      LOGICAL, INTENT (IN) :: N
+      LOGICAL, INTENT (IN)  :: B
+   END FUNCTION LOGICAL_ADD
+END INTERFACE OPERATOR ( + )
+
 end module ex
