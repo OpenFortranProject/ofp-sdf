098eb954431537629399269d5ee02bbda70b27f8
rasmussn <rasmus@uoregon.edu>
2015-08-30 13:21:48 -0700

    Finished and moved upstairs.

--- tests/rule-tests/pp/new/R432.f90
+++
@@ -1,9 +0,0 @@
-!! R432 type-param-decl
-!    is type-param-name [ = scalar-int-initialization-expr ]
-
-TYPE big_deal(k, d)
-   INTEGER, KIND :: k = kind(0.0), d = 13
-   INTEGER (KIND=4), LEN :: d = 13
-end type
-
-end
