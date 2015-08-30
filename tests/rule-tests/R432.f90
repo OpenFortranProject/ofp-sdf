324672b1c7f2e5a44344b7d9ddede7ac78dc2428
rasmussn <rasmus@uoregon.edu>
2015-08-30 13:26:04 -0700

    Added l length param to allow to compile correctly.

--- tests/rule-tests/R432.f90
+++ tests/rule-tests/R432.f90
@@ -1,9 +1,9 @@
 !! R432 type-param-decl
 !    is type-param-name [ = scalar-int-initialization-expr ]
 
-TYPE big_deal(k, d)
+TYPE big_deal(k, d, l)
    INTEGER, KIND :: k = kind(0.0), d = 13
-   INTEGER (KIND=4), LEN :: d = 13
+   INTEGER (KIND=4), LEN :: l = 13
 end type
 
 end
