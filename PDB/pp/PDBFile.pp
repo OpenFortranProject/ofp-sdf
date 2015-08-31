96f4431d0159e1bd9bb3aaccda2d664a10917764
rasmussn <rasmus@uoregon.edu>
2015-08-30 19:36:45 -0700

    Initial version.

---
+++ PDB/pp/PDBFile.pp
@@ -0,0 +1,13 @@
+[
+   PDBFile                       -- V [_1 _2 _3],
+
+   FileHeader                    -- _1 _2,
+   Version                       -- H hs=0 ["<PDB " _1 ">"],
+   Language                      -- H ["lang" _1],
+
+   SourceFile                    -- H ["\nso#0" _1],
+
+   Routine                       -- H ["\nro#0" _1],
+
+   #EOF                          --
+]
