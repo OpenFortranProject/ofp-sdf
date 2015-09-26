b7a4472a93aff0c51b7c6cd7ed154ad1b8faca4a
rasmussn <rasmus@uoregon.edu>
2015-09-25 17:28:09 -0700

    Added the match and traverse args part.

--- ROSE/sage_traverse.pp
+++ ROSE/sage_traverse.pp
@@ -1,16 +1,22 @@
 [
    Traversals                          -- V [_1],
 
-   Traversal                           -- _1 V is=2 [
-                   "{"
-                        "#ifdef DEBUG_PRINT"
-                        "  printf(\"SgUntypedGlobalScope: %s\\n\", ATwriteToString(term));"
-                        "#endif\n"
-                        _2 _3 _4]
-                   "}\n",
+   Traversal                           -- _1 
+         "{"
+                                          V is=2 [_2 _3 _4 _5 _6]
+         "}\n",
 
-   FunDecl                             -- H hs=0 ["ATbool traverse_" _1
-                                                  "(ATerm term, sage::" _1 "** " _1 ")"],
+   FunDecl                             -- H hs=0 [
+         "//========================================================================================\n"
+         "// " _1 "\n"
+         "//----------------------------------------------------------------------------------------\n"
+         "ATbool traverse_" _1 "(ATerm term, sage::" _1 "** " _1 ")"
+                                                 ],
+
+   Debug                              -- H hs=0 [
+         "#ifdef DEBUG_PRINT\n"
+         "  printf(\"" _1 " %s\\n\", ATwriteToString(term));\n"
+         "#endif\n"                             ],
 
    TermDecls                           -- H hs=0 ["ATerm " _1 ";"],
    TermDecls.1:iter-sep                -- _1 ", ",
@@ -19,7 +25,19 @@
    ArgDecls                            -- _1,
    ArgDecl                             -- H hs=0 [_1 " " _2 ";"],
 
-   MatchStmt                           -- "\n" _1,
+   MatchConstruct                      -- H hs=0
+      [ 
+           "\n  if (ATmatch(term, \"" _1 "(" _2 ")\", " _3 ")) {"
+      ]
+        _4 "} else return ATfalse;\n",
+
+   MatchConstruct.2:iter-sep           -- _1 ",",
+   MatchConstruct.3:iter-sep           -- _1 ",",
+
+   TraverseArgs                        -- _1,
+   TraverseArg                         -- H hs=0
+      ["  if (traverse_"_1 "(" _2 ", &"_3 ")) {\n      // " _1 "\n    } else return ATfalse;"
+      ],
 
    TypeRef                             -- _1,
 
@@ -39,7 +57,9 @@
 
    Term                                -- _1,
 
-   TypePointer                         -- H hs=0 [_1 "*"],
+   TypePointer                         -- H hs=0 ["sage::" _1 "*"],
+
+   Address                             -- H hs=0 ["&" _1],
 
    Int                                 -- "int",
 
