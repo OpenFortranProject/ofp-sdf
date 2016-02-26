a95eac989695f2778fb4e345edd562913964e32e
rasmussn <rasmus@uoregon.edu>
2016-02-26 13:27:59 -0800

    Fixed to work with sage-to-traverse-header.

--- ROSE/sage_traverse_header.pp
+++ ROSE/sage_traverse_header.pp
@@ -1,39 +1,17 @@
 [
-   RTG                                 -- V [ "#ifndef TRAVERSE_SG_UNTYPED_NODES_HPP"
-                                              "#define TRAVERSE_SG_UNTYPED_NODES_HPP\n"
-                                              "#include <aterm2.h>"
-                                              "#include \"SgUntypedNodes.hpp\"\n"
+   Traversals                          -- V [
+                                               "#ifndef TRAVERSE_SG_UNTYPED_NODES_HPP"
+                                               "#define TRAVERSE_SG_UNTYPED_NODES_HPP\n"
+                                               _1
+                                               "\n#endif"
+                                            ],
 
-                                              _1 _2
+   Traversal                           -- _1,
 
-                                              "#endif // TRAVERSE_SG_UNTYPED_NODES_HPP"],
-
-   Start                               -- _1,
-
-   Nonterm                             -- _1,
-
-   Classes                             -- _1 "" _2 "" _3,
-
-   ClassDecl                           -- H hs=0 ["ATbool traverse_" _1
-                                                  "(ATerm, sage::" _1 "**);"],
-
-   ClassDef                            -- _1 _2 _3,
-
-   Constructor                         -- H hs=0 ["    " _1 "(" _2 ");"],
-   Constructor.2:iter-sep              -- _1 ", ",
-
-   Parent                              -- H [": " _1],
-   no-parent                           -- ,
-
-   Macro                               -- H ["#define" _1],
-
-   Ref                                 -- _1,
-
-   Term                                -- _1,
-
-   Int                                 -- "int",
-
-   String                              -- "std::string",
+   FunDecl                             -- H hs=0
+     [
+        "ATbool traverse_" _1 "(ATerm term, " _1 "** var_" _1 ");"
+     ],
 
    #end                                --
 ]
