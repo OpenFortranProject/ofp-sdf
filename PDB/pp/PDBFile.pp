a01d1445e46b84b55a39b86115d67519d35b742c
Soren Rasmussen <srasmus2@uoregon.edu>
2016-03-01 11:26:58 -0800

    Updated parse table. Should unparse entire ast correctly to produce a pdb file

--- PDB/pp/PDBFile.pp
+++ PDB/pp/PDBFile.pp
@@ -7,7 +7,22 @@
 
    SourceFile                    -- H ["\nso#0" _1],
 
-   Routine                       -- H ["\nro#0" _1],
+   routine                       -- H [_1 _2 _3 _4 _5 _6 _7 _8 _9],
+   ronum                         -- H ["\nro#" _1 _2],
+
+   so                            -- H ["so#0 " _1 _2],
+   st                            -- H hs=0["st#" _1],
+
+   stkind                        -- H [_1 _2 _3],
+   rsig                          -- H ["\nrsig ty#" _1],
+   rstart                        -- H ["\nrstart " _1],
+   rloc                          -- H ["\nrloc " _1],
+   rpos                          -- H ["\nrpos "_1 _1 _2 _2],
+   rlink                         -- H ["\nrlink " _1],
+   rkind                         -- H ["\nrkind " _1],
+   rstmt                         -- H ["\nrstmt " _1 _2 _3 _4],
+   rbody                         -- H ["\nrbody " _1],
+   NA                            -- H ["NA"],
 
    #EOF                          --
 ]
