fa5d701138351b5056d2bd27709c0c8879f83d86
rasmussn <rasmus@uoregon.edu>
2016-03-06 22:01:37 -0800

    Initial version.

---
+++ fortran/syntax/lex/FreeForm.pp
@@ -0,0 +1,26 @@
+[
+   File                          -- H hs=0 [_1],
+
+   Comment                       -- _1 _2 _3,
+   FreeFormComment               -- _1 _2 _3 _4,
+   comchar                       -- _1,
+
+   Preprocess                    -- _1 _2 _3,
+   prechar                       -- _1,
+
+   BlankLine                     -- _1 _2,
+
+   Stmt                          -- _1 _2 _3 _4,
+   LabeledStmt                   -- _1 _2 _3 _4,
+   stchar                        -- _1,
+   ppchar                        -- _1,
+
+   Continuation                  -- _1 _2 _3 _4,
+   conchar                       -- _1,
+
+   eol                           -- _1,
+
+   ws                            -- _1,
+   no-ws                         --
+
+]
