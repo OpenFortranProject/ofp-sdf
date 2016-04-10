f78807e4d7ccfde54107d3f6679ade446d435e57
rasmussn <rasmus@uoregon.edu>
2016-04-10 10:12:16 -0700

    Initial version.

---
+++ tests/rule-tests/pp/R568.f90
@@ -0,0 +1,6 @@
+COMMON // i
+COMMON /name1/ i1, i2
+10 COMMON // j2
+COMMON // i3, // j
+COMMON /name1/ i4, /name2/ j1, /name3/ k, // l
+END PROGRAM
---
+++ tests/rule-tests/pp/R569.f90
@@ -0,0 +1,4 @@
+INTEGER, PARAMETER :: r = 13
+COMMON // truth, beauty, ugly, lies
+COMMON // foo(3:8,4,5+r)
+END PROGRAM
