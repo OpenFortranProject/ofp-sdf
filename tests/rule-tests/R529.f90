7d0e8bf15f17d7e03f43df8cf9e715f8a63cc68c
rasmussn <rasmus@uoregon.edu>
2016-04-10 13:29:02 -0700

    Changed to make compilable.

--- tests/rule-tests/R529.f90
+++ tests/rule-tests/R529.f90
@@ -1,8 +1,9 @@
 !! R529 bind-stmt
 
-10  bind(c) a
-    bind(C, name='binky') i, j
+10  bind(c) i, j
+    bind(C, name='binky') zztop
 
+COMMON /common_block_name/ zztop
 BIND(C) :: B, C, /common_block_name/
 
 end
