56314e0c47d9182583c42306bd9bb0454309f71a
rasmussn <rasmus@uoregon.edu>
2016-06-06 15:05:25 -0600

    Initial version.

---
+++ tests/fixed-form-tests/starting-tabs.for
@@ -0,0 +1,21 @@
+! tab to start a statement
+	integer i
+
+! tab following label
+1	continue
+12	continue
+123	continue
+1234	continue
+12345	continue
+
+! label - space(s) - tab
+2	continue
+3  	continue
+22 	continue
+223 	continue
+2234 	continue
+22345 	continue
+
+! normal (no tabs)
+      end
+
