f50cd35ca2962c1a1e663a33b7488585d764bbfb
rasmussn <rasmus@uoregon.edu>
2016-11-19 12:59:32 -0800

    Uncommented .unaryop. and .binaryop. tests.

---
+++ tests/rule-tests/R310.f90
@@ -0,0 +1,14 @@
+!! R310 defined-operator
+!    is defined-unary-op        R703
+!    or defined-binary-op       R723
+!    or extended-intrinsic-op   R311
+
+interface operator(.unaryop.)
+end interface operator(.unaryop.)
+interface operator(.binaryop.)
+end interface operator(.binaryop.)
+interface operator(**)
+end interface operator(**)
+interface operator(//)
+end interface operator(//)
+end
