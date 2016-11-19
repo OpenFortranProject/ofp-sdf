19889d4cf74809c832e799930deaf0ff00d4e75f
rasmussn <rasmus@uoregon.edu>
2016-11-19 09:26:07 -0800

    Added more digits to some labels and used 0.

--- tests/rule-tests/R312.f90
+++ tests/rule-tests/R312.f90
@@ -3,13 +3,14 @@
 !
 ! Test label_list with a list of length 1,2,3.
 !
-GOTO (555) iblah
-GO TO (555,666) iblah
-goto (314,15,9), iblah
+GOTO (0555) iblah
+GO TO (550,01234) iblah
+goto (0314,15,9), iblah
 
 9   continue
 15  continue
-314 continue
-555 continue
+0314 continue
+0555 continue
+550  continue
 
-666 end
+01234 end
