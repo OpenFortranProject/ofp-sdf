214823cb6badf34e0826bbd599fad980e07922a7
rasmussn <rasmus@uoregon.edu>
2016-12-28 07:34:17 -0800

    Moved R925.f90 to R926.f90

--- tests/rule-tests/R926.f90
+++ tests/rule-tests/R926.f90
@@ -1,9 +1,12 @@
-!! R925 rewind-stmt
+!! R926 rewind-stmt
 !    is  REWIND file-unit-number
 !    or  REWIND ( position-spec-list )
 !
 ! Not tested here: file-unit-number and position-spec-list.
-rewind 10
+
+11 REWIND(unitnumber)
+rewind unitnumber
+12 rewind 10
 rewind (10, iostat=n, err=4)
 
 4 end
