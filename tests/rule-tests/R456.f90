3bcfbe8c677848ab6a4bc3be16546909b73847e4
rasmussn <rasmus@uoregon.edu>
2016-12-28 16:09:41 -0800

    Added implementation of test by utilizing notes 4.59 and 4.60.

--- tests/rule-tests/R456.f90
+++ tests/rule-tests/R456.f90
@@ -1,12 +1,34 @@
-!! R460 component-spec
+!! R456 component-spec
 !   is [ keyword = ] component-data-source
 
-! Testing component_spec_list.
-! I haven't been able to trigger this. The following is just the code
-! for R458 type_param_spec_list.
+13 type person
+   integer age
+   character (LEN = 50) name
+14 END TYPE person
 
-!TODO-F08 type(foo(5)) :: bar
-!TODO-F08 type(foo(dim=a)) :: abar
-!TODO-F08 type(truth(3,4,ugly=5)) :: beauty
+type point                            ! A base type
+  REAL :: X, Y
+END TYPE POINT
+TYPE, EXTENDS(POINT) :: COLOR_POINT   ! An extension of TYPE(POINT)
+  ! Components X and Y, and component name POINT, inherited from parent
+  INTEGER :: COLOR
+END TYPE COLOR_POINT
 
-END
+type(person) p
+
+!! NOTE 4.59
+!
+15 type(point) :: PV = POINT(1.0, 2.0)
+type(color_point) :: cp1, cp2, cp3
+
+cp1 = COLOR_POINT( point=point(1,2), color=3)
+cp2 = COLOR_POINT( point=PV, color=3)
+cp3 = COLOR_POINT(1 , 2, 3)
+
+!! NOTE 4.60
+!
+16 p = PERSON (21, 'JOHN SMITH')
+! This could also be written as
+p = PERSON (NAME = 'JOHN SMITH', AGE = 21)
+
+end
