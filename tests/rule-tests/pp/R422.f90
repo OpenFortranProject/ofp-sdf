e1164af06e8cbe2999e599b2dc46aa0532878662
rasmussn <rasmus@uoregon.edu>
2016-12-31 16:41:50 -0800

    Initial version.

---
+++ tests/rule-tests/pp/R422.f90
@@ -0,0 +1,10 @@
+SUBROUTINE whoot(str_star)
+INTEGER, PARAMETER :: a = 13
+CHARACTER(1) :: str1
+CHARACTER(2) :: str2
+CHARACTER(3) :: str3
+CHARACTER(a) :: stra
+CHARACTER(*) :: str_star
+CHARACTER(:), ALLOCATABLE :: str_colon
+END SUBROUTINE
+
