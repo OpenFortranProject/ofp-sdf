c2afb4ed9fc418877c9e5be9192e13409a2e52bd
rasmussn <rasmus@uoregon.edu>
2016-12-31 17:01:00 -0800

    Initial version.

---
+++ tests/rule-tests/R422.f90
@@ -0,0 +1,15 @@
+!! R422 char-length
+!    is ( type-param-value )
+!￼￼￼￼or int-literal-constant
+
+subroutine whoot(str_star)
+integer, parameter :: a = 13
+
+character*1    :: str1
+character*2,      str2
+character*(3)  :: str3
+character*(a)  :: stra
+character*(*)  :: str_star
+character*(:), allocatable :: str_colon
+
+end subroutine
