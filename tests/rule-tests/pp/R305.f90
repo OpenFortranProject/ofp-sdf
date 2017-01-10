deed64e5234a393485f5f2acb98170bb1eb14283
rasmussn <rasmus@uoregon.edu>
2017-01-10 14:12:38 -0800

    Initial version.

---
+++ tests/rule-tests/pp/R305.f90
@@ -0,0 +1,16 @@
+COMPLEX :: c
+LOGICAL :: l
+CHARACTER(5) :: str
+i = 13
+i = 13_4
+r = 3.1415
+r = 3.1415_8
+c = (-1.0_4,+2.0_4)
+l = .TRUE.
+l = .FALSE._4
+str = "hello"
+str = 1_"hello"
+z = Z"abcdef"
+o = O'01234567'
+b = B'0110'
+END PROGRAM
