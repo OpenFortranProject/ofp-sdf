2b053a2a3527473c3d8a0be06b5e752f353125b6
rasmussn <rasmus@uoregon.edu>
2017-01-11 11:14:48 -0800

    Initial version.

--- tests/rule-tests/R503.f90
+++ tests/rule-tests/R503.f90
@@ -4,9 +4,10 @@
 !                   [ * char-length ] [ initialization ]
 !    or function-name [ * char-length ]
 
-!TODO-F08 - improve test e.g. for co-array-spec
-
+integer :: i(13)
 integer x,y,z,what,ever
 real truth, beauty
+CHARACTER :: this*35, ridiculous(5,6)*12
+character, parameter :: p1*(*) = "x", p2*(*) = "y"
 
 end
