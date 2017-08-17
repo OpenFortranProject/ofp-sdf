2e2ab342fd13e77d8ffd509e1117d5a91a886bde
Craig Rasmussen <rasmus@uoregon.edu>
2017-08-17 16:39:20 -0700

    Added testing of optional parts (spec and submodule).

--- tests/rule-tests/R1116.f90
+++ tests/rule-tests/R1116.f90
@@ -4,5 +4,29 @@
 !           [ module-subprogram-part ]
 !        end-submodule-stmt
 !
-SUBMODULE (parent) submod
-END SUBMODULE submod
+module ancestor
+end
+
+SUBMODULE (ancestor) submod1
+END SUBMODULE submod1
+
+SUBMODULE (ancestor) submod2
+  contains
+END SUBMODULE
+
+SUBMODULE (ancestor) submod2
+  integer :: i
+END SUBMODULE
+
+SUBMODULE (ancestor) submod3
+  contains
+  subroutine bleu
+  end
+END
+
+SUBMODULE (ancestor) submod4
+  integer i
+  contains
+  subroutine bleu
+  end
+END
