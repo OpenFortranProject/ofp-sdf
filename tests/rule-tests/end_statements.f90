222b0dd29df842de1bc71580a289802a2d9fda9b
Craig Rasmussen <rasmus@uoregon.edu>
2017-08-25 14:51:08 -0700

    Initial version.  Added new F2008 standard tests to fulfill ECP deliverable.

---
+++ tests/rule-tests/end_statements.f90
@@ -0,0 +1,21 @@
+program contains
+contains
+  subroutine contains_sub
+    contains
+  end
+  function contains_func()
+    contains
+  end
+end
+
+module contains_mod
+contains
+  subroutine contains_sub
+    contains
+  end
+  function contains_func()
+     contains
+  end
+end
+
+
---
+++ tests/rule-tests/pp/empty_contains.f90.no-diff
@@ -0,0 +1,15 @@
+program contains
+  contains
+end
+
+module contains_mod
+  contains
+end
+
+subroutine contains_sub
+  contains
+end
+
+function contains_func()
+  contains
+end
