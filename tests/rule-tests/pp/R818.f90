699aeb15ef494738d6c79c138b20b1d4874cee48
Craig Rasmussen <rasmus@uoregon.edu>
2017-08-25 15:02:04 -0700

    Initial version of pp tests derived from original rule-test.

---
+++ tests/rule-tests/pp/R818.f90
@@ -0,0 +1,5 @@
+REAL :: A(10), B(10)
+DO CONCURRENT(I=1:N)
+  A(I) = B(I)
+END DO 
+END PROGRAM
---
+++ tests/rule-tests/pp/R855.f90
@@ -0,0 +1,10 @@
+INTEGER, PARAMETER :: stop_code = 999
+CHARACTER, PARAMETER :: char_stop = "stop please"
+STOP
+13 STOP 'end of program'
+STOP 1
+STOP stop_code
+STOP 123
+STOP 1234
+STOP char_stop
+END PROGRAM
