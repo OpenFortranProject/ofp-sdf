1c7c7b7b6091da42863b54687873e91c950ebdcb
Craig Rasmussen <rasmussen17@llnl.gov>
2018-03-13 11:45:33 -0700

    (syntax) Finished additions to CUDA syntax.
      * primarily regarding prefix spec
      * also cleaned up a test file (R515.f90)

--- tests/rule-tests/R515.f90
+++ tests/rule-tests/R515.f90
@@ -5,11 +5,11 @@
 !    or assumed-size-spec
 !    or implied-shape-spec -list
 
-real x(314159)
-integer y(31,41,59,2:6,5)
-integer bar(2:6,5:*)
+subroutine test_array_spec(bar,truth,beauty,ugly)
+  real x(314159)
+  integer y(31,41,59,2:6,5)
+  integer bar(2:6,5:*)
 3 Real truth(31,41,*)
-integer beauty(31:,41:)
-integer ugly(:,:)
-
+  integer beauty(31:,41:)
+  integer ugly(:,:)
 end
